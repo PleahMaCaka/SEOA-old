900b327639d809a5861c34b898e414114c9db7d8
PleahMaCaka <kkjuh0905@gmail.com>
2022-06-06 02:00:28 +0900

    feat(slash-about): new!

--- src/commands/infomation/About.ts
+++ src/commands/infomation/About.ts
@@ -1,25 +1,16 @@
 import { CommandInteraction, MessageActionRow, MessageButton, MessageEmbed } from "discord.js";
 import { Client, Discord, Slash } from "discordx";
 import { MessageButtonStyles } from "discord.js/typings/enums";
+import { author, repository } from "../../../package.json";
 
 @Discord()
 export abstract class About {
 
-	@Slash("about", {
-		nameLocalizations: {
-			ko: "정보"
-		},
-		description: "A description of this bot!",
-		descriptionLocalizations: {
-			ko: "봇의 정보와 상태 그리고 초대 링크를 확인하세요!"
-		}
-	})
+	@Slash("정보", { description: "봇에 대해 알아보세요!", })
 	private async aboutSlash(
 		interaction: CommandInteraction,
 		client: Client
-	): Promise<void> {
-		const { description, repository } = require("../../../package.json")
-
+	) {
 		//////////////////////////////
 		// EMBED
 		////////////////////
@@ -27,7 +18,11 @@ export abstract class About {
 		const about = new MessageEmbed()
 			.setColor("#bc92ff")
 			.setTitle(`About ${client.user!.username}`)
-			.setDescription(`${client.user!.username} is ${description}`)
+			.setDescription(`다른 서버에 초대하고 싶나요? \`/초대\``)
+			.setThumbnail("https://cdn.discordapp.com/attachments/982752265418977320/982752400135819274/TS_LOGO.png")
+			.addField("공식 문서", "준비중 (GitBook)", true)
+			.addField("지원 서버", "[접속하기](https://discord.gg/YGmyFer)", true)
+			.addField("개발자", `<@${process.env.ADMIN}> (${author.name})`, false)
 
 		//////////////////////////////
 		// BUTTON
