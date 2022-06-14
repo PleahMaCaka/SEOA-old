7c4e6193394fe16f0165c289f982b920a0a4711b
PleahMaCaka <kkjuh0905@gmail.com>
2022-06-15 04:29:37 +0900

    feat(InteractionHandler): btn interaction

--- src/events/InteractionHandler.ts
+++ src/events/InteractionHandler.ts
@@ -8,16 +8,12 @@ class InteractionHandler {
 	// Slash Interaction
 	////////////////////
 	@On("interactionCreate")
-	private async slashInteraction(
-		[interaction]: ArgsOf<"interactionCreate">,
-		client: Client
-	) {
+	private async slashInteraction([interaction]: ArgsOf<"interactionCreate">, client: Client) {
 		if (!interaction.isCommand()) return
-
 		try {
 			await client.executeInteraction(interaction)
 		} catch (e) {
-			Logger.log("ERROR", `Cannot execute slash command -> ${interaction.commandName} <- check the command.`)
+			Logger.log("ERROR", `Cannot execute [slash] interaction -> ${interaction.commandName} <- check the command.`)
 			console.log(e)
 		}
 	}
@@ -26,16 +22,26 @@ class InteractionHandler {
 	// Context Interaction
 	////////////////////
 	@On("interactionCreate")
-	private async interaction(
-		[interaction]: ArgsOf<"interactionCreate">,
-		client: Client
-	): Promise<void> {
+	private async contextInteraction([interaction]: ArgsOf<"interactionCreate">, client: Client) {
 		if (!interaction.isContextMenu()) return
-
 		try {
 			await client.executeInteraction(interaction)
 		} catch (e) {
-			Logger.log("ERROR", `Cannot execute interaction-> ${interaction.commandName} <- check the command.`)
+			Logger.log("ERROR", `Cannot execute [context] interaction-> ${interaction.commandName} <- check the command.`)
+			console.log(e)
+		}
+	}
+
+	//////////////////////////////
+	// Button Interaction
+	////////////////////
+	@On("interactionCreate")
+	private async buttonInteraction([interaction]: ArgsOf<"interactionCreate">, client: Client) {
+		if (!interaction.isButton()) return
+		try {
+			await client.executeInteraction(interaction)
+		} catch (e) {
+			Logger.log("ERROR", `Cannot execute [button] interaction -> ${interaction.customId} <- check the command.`)
 			console.log(e)
 		}
 	}
