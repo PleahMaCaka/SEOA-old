5535a2b32659dfab1fa37cd67b77ad0af882e6a5
pleahmacaka <kkjuh0905@gmail.com>
2022-06-25 22:48:48 +0900

    :tea: mongodb connect with user & pass

--- src/mongo/Mongo.ts
+++ src/mongo/Mongo.ts
@@ -1,28 +1,41 @@
 import { Interaction } from "discord.js"
 import { connect } from "mongoose"
+import { DEBUG } from "../start";
 import { Logger } from "../utils/Logger"
-import { user } from "./models/UserData"
+import { user } from "./models/UserData";
+
+
+const DB_URI = process.env.DB_URI ? process.env.DB_URI : "mongodb://localhost:27017"
+const DB_NAME = process.env.DB_NAME ? process.env.DB_NAME : "discord"
+const DB_USER = process.env.DB_URI === "mongodb://localhost:27017" ? undefined : process.env.DB_USER
+const DB_PASS = process.env.DB_PASS === "mongodb://localhost:27017" ? undefined : process.env.DB_PASS
+
 
 export async function connectMongoDB() {
-	let uri = process.env.DB_URI ? process.env.DB_URI : "mongodb://127.0.0.1:27017"
-	if (uri.endsWith("/")) uri = uri + "discord"
-	else uri = uri + "/discord"
-
-	if (uri.indexOf("localhost")) {
+	if (DB_URI.indexOf("localhost") != -1) {
 		Logger.log("WARN",
 			"You have set your MongoDB URI to localhost. But it is changed to \"127.0.0.1\" and connected " +
 			"because it cause problems depending on the version of Node.")
-		uri.replace("localhost", "127.0.0.1")
+		DB_URI.replace("localhost", "127.0.0.1")
 	}
 
-	await connect(uri,
-		(error) => {
-			if (error) {
-				Logger.log("ERROR", "MongoDB connection error: " + error)
-				throw new Error()
-			}
-			else Logger.log("INFO", "MongoDB Connected!")
-		})
+	if (DB_URI != "mongodb://localhost:27017")
+		if (!DB_URI || !DB_USER || !DB_PASS)
+			throw new Error("You have to set your MongoDB URI, username and password in your env (or .env file)")
+
+	if (DB_URI.indexOf("127.0.0.1") != -1 && !DEBUG)
+		Logger.log("ERROR", "Connect to localhost DB in non-debug mode.")
+
+	// local mongodb
+	if (DB_URI == "mongodb://127.0.0.1:27017")
+		return await connect(DB_URI, { dbName: DB_NAME ? DB_NAME : "discord" })
+			.then(() => Logger.log("INFO", "Connected to localhost(127.0.0.1) MongoDB"))
+
+	// external mongodb
+	else if (DB_USER && process.env.DB_PASS) {
+		await connect(DB_URI, { user: DB_USER, pass: DB_PASS, dbName: DB_NAME })
+			.then(() => Logger.log("INFO", `Connected to MongoDB ${process.env.DB_URI} - [USER : ${process.env.DB_USER}]`))
+	}
 }
 
 //////////////////////////////
--- src/typescript/env.d.ts
+++ src/typescript/env.d.ts
@@ -1,11 +1,16 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
+			// Dev & Admin
 			TOKEN: string | undefined
 			ADMIN: string | undefined
 			ADMIN_GUILD: string | undefined
 			DEBUG: string | undefined
+			// MongoDB
 			DB_URI: string | undefined
+			DE_NAME: string | undefined
+			DB_USER: string | undefined
+			DB_PASS: string | undefined
 		}
 	}
 }
